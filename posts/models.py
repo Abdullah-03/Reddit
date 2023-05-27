dccc94dee5d7448705664d8a93ebbb80c4e82812
Darana <abdullahsaud03@gmail.com>
2023-05-28 02:56:21 +0500

    linked post model to custom user

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+from profiles.models import CustomUser
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     karma = models.IntegerField(verbose_name="post karma", default=1)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
 
